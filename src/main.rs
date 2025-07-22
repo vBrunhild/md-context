162ae08d47a6bb4229724f5107e1878b054b5406
vBrunhild <brunomoretti100@gmail.com>
2025-07-22 01:50:11 +0000

    feat(cli): create add command

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,50 @@
-mod configuration;
-
-use std::env;
+use std::{fs, path::PathBuf};
 use clap::Parser;
 
+#[derive(Parser)]
+#[command(name = "mdcon")]
+enum Cli {
+    Add(AddArgs)
+}
 
-
-
-
-#[derive(Debug, Parser)]
+#[derive(Parser)]
 #[command(version, about, long_about = None)]
-struct Args {
-    #[arg(short, long)]
-    name: String
+struct AddArgs {
+    paths: Vec<PathBuf>
 }
 
 
+#[derive(Debug, Clone)]
+enum Path {
+    Dir { buf: PathBuf, content: Vec<Path> },
+    File { buf: PathBuf, content: Option<String> }
+}
+
+fn buf_to_path(buf: PathBuf) -> Option<Path> {
+    if buf.is_file() {
+        let content = fs::read_to_string(&buf).ok();
+        Some(Path::File { buf, content })
+    } else if buf.is_dir() {
+        let content = fs::read_dir(&buf)
+            .expect("Should always be a dir")
+            .flat_map(|inner| match inner {
+                Ok(dir) => buf_to_path(dir.path()),
+                Err(_) => None
+            })
+            .collect();
+
+        Some(Path::Dir { buf, content })
+    } else {
+        None
+    }
+}
+
 fn main() {
-    
-    let args = Args::parse();
-    println!("yay {}!", args.name);
+    let Cli::Add(args) = Cli::parse();
+    let paths: Vec<Path> = args.paths.into_iter()
+        .flat_map(|buf| buf_to_path(buf))
+        .collect();
+
+    dbg!(paths);
 }
 
