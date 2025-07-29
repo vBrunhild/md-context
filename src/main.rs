b3c504e4d64b4034f96fc971f477bd64c88adfb7
vBrunhild <brunomoretti100@gmail.com>
2025-07-29 04:07:14 +0000

    feat: create many different prototype types

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,114 @@
-use std::{fs, path::PathBuf};
+use std::{fs, sync::mpsc, convert::TryFrom, fmt::Display, marker::PhantomData, path::PathBuf};
 use clap::Parser;
+use threadpool::ThreadPool;
 
-#[derive(Parser)]
+#[derive(Debug, Parser)]
 #[command(name = "mdcon")]
 enum Cli {
-    Add(AddArgs)
+    Gen(GenArgs)
 }
 
-#[derive(Parser)]
+#[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
-struct AddArgs {
+struct GenArgs {
     paths: Vec<PathBuf>
 }
 
+trait PathType {}
+
+struct File {}
+impl PathType for File {}
+
+struct Dir {}
+impl PathType for Dir {}
 
 #[derive(Debug, Clone)]
-enum Path {
-    Dir { buf: PathBuf, content: Vec<Path> },
-    File { buf: PathBuf, content: Option<String> }
+struct Path<T: PathType> {
+    buf: PathBuf,
+    _type: PhantomData<T>
 }
 
-fn buf_to_path(buf: PathBuf) -> Option<Path> {
-    if buf.is_file() {
-        let content = fs::read_to_string(&buf).ok();
-        Some(Path::File { buf, content })
-    } else if buf.is_dir() {
-        let content = fs::read_dir(&buf)
-            .expect("Should always be a dir")
-            .flat_map(|inner| match inner {
-                Ok(dir) => buf_to_path(dir.path()),
-                Err(_) => None
-            })
-            .collect();
+impl Path<Dir> {
+    fn read(&self) -> fs::ReadDir {
+        fs::read_dir(&self.buf).expect("should always be directory")
+    }
+}
 
-        Some(Path::Dir { buf, content })
-    } else {
-        None
+impl Path<File> {
+    fn read(&self) -> String {
+        fs::read_to_string(&self.buf).expect("should always be readable file")
+    }
+}
+
+impl<T: PathType> Display for Path<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.buf.display())
+    }
+}
+
+impl TryFrom<PathBuf> for Path<Dir> {
+    type Error = BufToPathError;
+
+    fn try_from(value: PathBuf) -> Result<Self, Self::Error> {
+        if value.is_dir() {
+            Ok(Self { buf: value, _type: PhantomData::<_> })
+        } else {
+            Err(BufToPathError::Dir(value))
+        }
+    }
+}
+
+impl TryFrom<PathBuf> for Path<File> {
+    type Error = BufToPathError;
+
+    fn try_from(value: PathBuf) -> Result<Self, Self::Error> {
+        if value.is_file() {
+            Ok(Self { buf: value, _type: PhantomData::<_> })
+        } else {
+            Err(BufToPathError::File(value))
+        }
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+enum BufToPathError {
+    #[error("Error when converting buf to directory: {0}")]
+    Dir(PathBuf),
+    #[error("Error when converting buf to file: {0}")]
+    File(PathBuf)
+}
+
+enum PathTree<'a> {
+    Dir(&'a Path<Dir>, Vec<&'a PathTree<'a>>),
+    File(&'a Path<File>)
+}
+
+impl<'a> PathTree<'a> {
+    fn from_dir(path: &'a Path<Dir>) -> Self {
+        Self::Dir(path, Vec::new())
+    }
+
+    fn from_file(path: &'a Path<File>) -> Self {
+        Self::File(path)
     }
 }
 
 fn main() {
-    let Cli::Add(args) = Cli::parse();
-    let paths: Vec<Path> = args.paths.into_iter()
-        .flat_map(|buf| buf_to_path(buf))
-        .collect();
+    let pool = ThreadPool::new(8);
+    let Cli::Gen(args) = Cli::parse();
 
-    dbg!(paths);
+    let (input, output) = mpsc::channel();
+    for buf in args.paths {
+        let input = input.clone();
+        pool.execute(move || {
+            let path: Path<File> = buf.try_into().unwrap();
+            input.send(path).unwrap();
+        });
+    }
+
+    drop(input);
+
+    for path in output {
+        println!("{path}");
+    }
 }
-
