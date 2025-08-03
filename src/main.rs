b7de040d52bcac6625746a8c724cc7dab4609a53
vBrunhild <brunomoretti100@gmail.com>
2025-08-03 23:41:57 +0000

    refactored to work without thread pool

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::{fs, path::PathBuf, sync::{atomic::{AtomicU32, Ordering}, Arc}};
+use std::{fs, path::PathBuf, sync::{atomic::{AtomicU32, Ordering}}, thread};
 use clap::Parser;
-use threadpool::ThreadPool;
 
 #[derive(Debug, Parser)]
 #[command(name = "mdcon")]
@@ -16,46 +15,44 @@ struct GenArgs {
 
 fn main() {
     let workers = 8;
-    let pool = ThreadPool::new(workers);
     let Cli::Gen(args) = Cli::parse();
 
     let (sender, receiver) = crossbeam_channel::unbounded();
-    let pending = Arc::new(AtomicU32::new(0));
+    let pending = AtomicU32::new(0);
 
-    for buf in args.paths {
+    for path in args.paths {
         pending.fetch_add(1, Ordering::Relaxed);
-        sender.send(Some(buf)).unwrap();
+        sender.send(Some(path)).expect("should send arg paths");
     }
 
-    for _ in 0..workers {
-        let sender = sender.clone();
-        let receiver = receiver.clone();
-        let pending = pending.clone();
+    thread::scope(|s| { 
+        for _ in 0..workers {
+            s.spawn(|| {
+                let sender = sender.clone();
+                let receiver = receiver.clone();
 
-        pool.execute(move || {
-            while let Ok(Some(path)) = receiver.recv() {
-                if path.is_dir() {
-                    if let Ok(dir) = fs::read_dir(&path) {
-                        dir.for_each(|dir_entry| match dir_entry {
-                            Ok(dir_entry) => {
-                                pending.fetch_add(1, Ordering::Relaxed);
-                                sender.send(Some(dir_entry.path())).unwrap();
-                            },
-                            Err(_) => eprintln!("error reading dir"),
-                        });
+                while let Ok(Some(path)) = receiver.recv() {
+                    if path.is_dir() {
+                        if let Ok(dir) = fs::read_dir(&path) {
+                            dir.for_each(|dir_entry| match dir_entry{
+                                Ok(dir_entry) => {
+                                    pending.fetch_add(1, Ordering::Relaxed);
+                                    sender.send(Some(dir_entry.path())).expect("should send path");
+                                },
+                                Err(_) => eprintln!("error reading dir"),
+                            });
+                        }
+                    } else if path.is_file() {
+                        println!("found file {}", path.display());
                     }
-                } else if path.is_file() {
-                    println!("{} - {}", path.display(), fs::read_to_string(&path).unwrap_or_else(|_| "err".into()));
-                }
 
-                if pending.fetch_sub(1, Ordering::Relaxed) == 1 {
-                    for _ in 0..workers {
-                        sender.send(None).unwrap();
+                    if pending.fetch_sub(1, Ordering::Relaxed) == 1 {
+                        for _ in 0..workers {
+                            sender.send(None).expect("should send poison pill");
+                        }
                     }
                 }
-            }
-        });
-    }
-
-    pool.join();
+            });
+        }
+    });
 }
